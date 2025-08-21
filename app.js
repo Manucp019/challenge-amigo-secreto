c20537307388c4e4afece2b56a18150f5ebbde5d
Manucp019 <manuelaceballospardo@gmail.com>
2025-08-21 14:22:38 -0300

    creo app.js

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim();
+    if (nombre !== '') {
+        amigos.push(nombre);
+        mostrarLista();
+        input.value = '';
+        input.focus();
+    }
+}
+
+function mostrarLista() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    amigos.forEach((amigo) => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    const resultado = document.getElementById('resultado');
+    if (amigos.length === 0) {
+        resultado.innerHTML = '<li>No hay amigos para sortear.</li>';
+        return;
+    }
+    // Elegir un índice aleatorio
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    // Mostrar el resultado en la lista de resultados
+    resultado.innerHTML = `<li>El amigo sorteado es: ${amigoSorteado}</li>`;
+}
+
